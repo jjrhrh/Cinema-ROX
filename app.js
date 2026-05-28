df5e805e3963d845cb51d9fbc6edae3aa7eb78df
جواد <89310596+jjrhrh@users.noreply.github.com>
2026-05-28 23:53:07 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1975,6 +1975,7 @@ const superflixUrl = type === 'movie'
   : `https://superflixapi.best/serie/${id}/${season}/${episode}`;
 
 const vipSrvs = [
+  { icon:'<i class="ri-shield-star-fill style-icon" style="color:#7c3aed"></i>', name:'STREAMVAULT', desc:'VIP • 12 مصدر', url:srvUrl(S.SRV_SV,type,id,season,episode) },
   { icon:'<i class="ri-flashlight-2-fill style-icon" style="color:#00e5ff"></i>', name:'EZVID', desc:'VIP • 8 مصادر HLS', url: type==='movie' ? `https://ezvidapi.com/embed/movie/${id}` : `https://ezvidapi.com/embed/tv/${id}/${season}/${episode}` },
   { icon:'<i class="ri-global-fill style-icon" style="color:#00cec9"></i>', name:'SUPER', desc:'VIP • HLS سريع', url: type==='movie' ? `https://multiembed.mov/directstream.php?video_id=${id}&tmdb=1` : `https://multiembed.mov/directstream.php?video_id=${id}&tmdb=1&s=${season}&e=${episode}` },
   { icon:'<i class="ri-vip-diamond-fill style-icon" style="color:#a855f7"></i>', name:'VIDLUX', desc:'VIP • 9 سيرفرات', url:srvUrl(S.SRV_VIDLUX,type,id,season,episode) + '?color=e50914' },
